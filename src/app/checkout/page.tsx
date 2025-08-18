a0047b0b9dcdc43aaabdb3d00cae7bfaf71fb0d8
rahulkadiwala <rahulkadiwala0092@gmail.com>
2025-08-18 11:28:14 +0530

    api local host remove

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -52,7 +52,7 @@ export default function CheckOutPage() {
     };
     console.log(order);
     try {
-      const res = await fetch("http://localhost:3000/api/orders", {
+      const res = await fetch("/api/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
